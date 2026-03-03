31ca49e671abfcc917ea030947ed91d2aa11e218
planetnxa <marialarte26@gmail.com>
2026-03-03 01:25:19 +0000

    db-init

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+/* data to read
+
+ https://blog.intertoons.com/simplified-guide-to-documenting-asp-net-web-api-with-swagger-f4c40731f90c
+https://learn.microsoft.com/en-us/aspnet/core/tutorials/web-api-help-pages-using-swagger?view=aspnetcore-8.0
+https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-10.0&tabs=visual-studio#test-the-get-endpoints
+ */
 // Add services to the container.
 
 builder.Services.AddControllers();
