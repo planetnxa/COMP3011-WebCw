3fa3f9a00500065a29aa8377307ad0496e3ad3a7
planetnxa <sc22amll@leeds.ac.uk>
2026-03-18 05:09:43 +0000

    init edits

--- Controllers/FragrancesController.cs
+++ Controllers/FragrancesController.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using WebAppComp3011.Data;
 using WebAppComp3011.Models;
-using Microsoft.Data.SqlClient;
-using System.Data;
-using System.Reflection;
-using Microsoft.Data.Sqlite;
-using Newtonsoft.Json;
 
 namespace WebAppComp3011.Controllers
 {
@@ -29,6 +30,7 @@ namespace WebAppComp3011.Controllers
 
 
         // GET: api/Fragrances
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Fragrance>>> GetFragrances()
         {
@@ -37,33 +39,40 @@ namespace WebAppComp3011.Controllers
             {
                 await cn.OpenAsync();
                 var cmd = cn.CreateCommand();
-                // include sqlite internal rowid so we can use it as the Fragrance.Id
-                cmd.CommandText = "SELECT rowid, * FROM perfume100"; // made smaller db table for testing
+                // read from perf100 table (schema provided by user)
+
+                cmd.CommandText = "SELECT * FROM perfumeData";
+
                 using (var reader = await cmd.ExecuteReaderAsync()) {
                     int p = 0;
                     while (await reader.ReadAsync())
                     {
-                        Console.WriteLine(reader["Perfume"].ToString());
                         p += 1;
-                        Fragrance f = new Fragrance() {
-                            Id = Convert.ToInt32(reader["rowid"]),
-                            FragUrl = reader["url"].ToString(),
-                            FragName = reader["Perfume"].ToString(),
-                            Brand = reader["Brand"].ToString(),
-                            Country = reader["Country"].ToString(),
-                            Gender = reader["Gender"].ToString(),
-                            RatingValue = float.Parse(reader["Rating Value"].ToString()), // fi fix with db in cw01
-                            RatingCount = Convert.ToInt32(reader["Rating Count"]),
-                            Year = reader["year"].ToString(),
-                            Notes = new Notes() { Base = reader["Base"].ToString(),
-                                Middle = reader["Middle"].ToString(),
-                                Top = reader["Top"].ToString() },
-                            Perfumer1 = reader["Perfumer1"].ToString(),
-                            Perfumer2 = reader["Perfumer2"].ToString(),
-                            MainAccords = new List<string> {reader["mainaccord1"].ToString(), reader["mainaccord2"].ToString() , reader["mainaccord3"].ToString() , reader["mainaccord4"].ToString() , reader["mainaccord5"].ToString() },
+                        var f = new Fragrance()
+                        {
+                            Id = Convert.ToInt32(reader["perfumeId"]),
+                            FragUrl = reader["url"]?.ToString(),
+                            FragName = reader["Perfume"]?.ToString(),
+                            Brand = reader["Brand"]?.ToString(),
+                            Country = reader["Country"]?.ToString(),
+                            Gender = reader["Gender"]?.ToString(),
+                            Rating = float.TryParse(reader["Rating"]?.ToString(), out var r) ? r : 0f,
+                            Year = reader["Year"]?.ToString(),
+                            Accords = reader["Accords"]?.ToString().Split("'").ToList(),
+                            Perfumers = reader["Perfumers"]?.ToString(),
+
+                            Notes = new Notes(){}
                         };
 
-                        list.Add(f);                   
+                        // try to populate notes from perfumeNotes table
+                        if (f.Id != 0)
+                        {
+                            var notes = await ReadNotes(f.Id);
+                            if (notes != null)
+                                f.Notes = notes;
+                        }
+
+                        list.Add(f);
                     }
                    // Console.WriteLine(reader);
                 } // await for async
@@ -73,6 +82,50 @@ namespace WebAppComp3011.Controllers
 
 
         }
+
+        private async Task<Notes> ReadNotes(int perfumeId)
+        {
+            var notes = new Notes() { 
+                Top = new List<string>(), 
+                Middle = new List<string>(), 
+                Base = new List<string>()
+            };
+
+            await using (var cn = new SqliteConnection(connectString))
+            {
+                await cn.OpenAsync();
+                var cmd = cn.CreateCommand();
+                cmd.CommandText = "SELECT Note, Type FROM perfumeNotes WHERE perfumeId = @id";
+                cmd.Parameters.AddWithValue("@id", perfumeId);
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        var note = reader["Note"]?.ToString();
+                        var type = reader["Type"]?.ToString();
+
+                        if (string.IsNullOrEmpty(note) || string.IsNullOrEmpty(type)) continue;
+                        switch (type.ToLowerInvariant())
+                        {
+                            case "top":
+                                notes.Top.Add(note);
+                                break;
+                            case "middle":
+                                notes.Middle.Add(note);
+                                break;
+                            case "base":
+                                notes.Base.Add(note);
+                                break;
+                        }
+                    }
+                }
+                await cn.CloseAsync();
+            }
+
+            return notes;
+        }
+
+        /*
         
         // GET: api/Fragrances/5
         [HttpGet("{id}")]
@@ -84,8 +137,8 @@ namespace WebAppComp3011.Controllers
             {
                 await cn.OpenAsync();
                 var cmd = cn.CreateCommand();
-                // use sqlite internal rowid as identifier for scaffolding
-                cmd.CommandText = "SELECT rowid, * FROM perfume100 WHERE rowid = @id LIMIT 1";
+                // select by perfumeId column from perf100 table
+                cmd.CommandText = "SELECT * FROM perf100 WHERE perfumeId = @id LIMIT 1";
                 cmd.Parameters.AddWithValue("@id", id);
                 using (var reader = await cmd.ExecuteReaderAsync())
                 {
@@ -93,32 +146,30 @@ namespace WebAppComp3011.Controllers
                     {
                         frag = new Fragrance()
                         {
-                            Id = Convert.ToInt32(reader["rowid"]),
-                            FragUrl = reader["url"].ToString(),
-                            FragName = reader["Perfume"].ToString(),
-                            Brand = reader["Brand"].ToString(),
-                            Country = reader["Country"].ToString(),
-                            Gender = reader["Gender"].ToString(),
-                            RatingValue = float.TryParse(reader["Rating Value"].ToString(), out var rv) ? rv : 0f,
-                            RatingCount = int.TryParse(reader["Rating Count"].ToString(), out var rc) ? rc : 0,
-                            Year = reader["year"].ToString(),
+                            Id = TryGetInt(reader, "perfumeId") ?? TryGetInt(reader, "rowid") ?? 0,
+                            FragUrl = reader["url"]?.ToString(),
+                            FragName = reader["Perfume"]?.ToString(),
+                            Brand = reader["Brand"]?.ToString(),
+                            Country = reader["Country"]?.ToString(),
+                            Gender = reader["Gender"]?.ToString(),
+                            Rating = float.TryParse(reader["Rating"]?.ToString(), out var r) ? r : 0f,
+                            Year = reader["Year"]?.ToString(),
+                            Accords = reader["Accords"]?.ToString(),
+                            Perfumers = reader["Perfumers"]?.ToString(),
                             Notes = new Notes()
                             {
-                                Base = reader["Base"].ToString(),
-                                Middle = reader["Middle"].ToString(),
-                                Top = reader["Top"].ToString()
-                            },
-                            Perfumer1 = reader["Perfumer1"].ToString(),
-                            Perfumer2 = reader["Perfumer2"].ToString(),
-                            MainAccords = new List<string>
-                            {
-                                reader["mainaccord1"].ToString(),
-                                reader["mainaccord2"].ToString(),
-                                reader["mainaccord3"].ToString(),
-                                reader["mainaccord4"].ToString(),
-                                reader["mainaccord5"].ToString()
+                                Top = string.Empty,
+                                Middle = string.Empty,
+                                Base = string.Empty
                             }
                         };
+
+                        if (frag.Id != 0)
+                        {
+                            var notes = await ReadNotesForPerfumeAsync(frag.Id);
+                            if (notes != null)
+                                frag.Notes = notes;
+                        }
                     }
                 }
                 await cn.CloseAsync();
@@ -143,46 +194,27 @@ namespace WebAppComp3011.Controllers
             {
                 await cn.OpenAsync();
                 var cmd = cn.CreateCommand();
-                cmd.CommandText = @"UPDATE perfume100 SET
+                cmd.CommandText = @"UPDATE perf100 SET
                     url = @FragUrl,
                     Perfume = @FragName,
                     Brand = @Brand,
                     Country = @Country,
                     Gender = @Gender,
-                    ""Rating Value"" = @RatingValue,
-                    ""Rating Count"" = @RatingCount,
-                    year = @Year,
-                    Top = @Top,
-                    Middle = @Middle,
-                    Base = @Base,
-                    Perfumer1 = @Perfumer1,
-                    Perfumer2 = @Perfumer2,
-                    mainaccord1 = @ma1,
-                    mainaccord2 = @ma2,
-                    mainaccord3 = @ma3,
-                    mainaccord4 = @ma4,
-                    mainaccord5 = @ma5
-                    WHERE rowid = @Id";
+                    Rating = @Rating,
+                    Year = @Year,
+                    Accords = @Accords,
+                    Perfumers = @Perfumers
+                    WHERE perfumeId = @Id";
 
                 cmd.Parameters.AddWithValue("@FragUrl", frag.FragUrl ?? string.Empty);
                 cmd.Parameters.AddWithValue("@FragName", frag.FragName ?? string.Empty);
                 cmd.Parameters.AddWithValue("@Brand", frag.Brand ?? string.Empty);
                 cmd.Parameters.AddWithValue("@Country", frag.Country ?? string.Empty);
                 cmd.Parameters.AddWithValue("@Gender", frag.Gender ?? string.Empty);
-                cmd.Parameters.AddWithValue("@RatingValue", frag.RatingValue);
-                cmd.Parameters.AddWithValue("@RatingCount", frag.RatingCount);
+                cmd.Parameters.AddWithValue("@Rating", frag.Rating);
                 cmd.Parameters.AddWithValue("@Year", frag.Year ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Top", frag.Notes?.Top ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Middle", frag.Notes?.Middle ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Base", frag.Notes?.Base ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Perfumer1", frag.Perfumer1 ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Perfumer2", frag.Perfumer2 ?? string.Empty);
-                var ma = frag.MainAccords ?? new List<string>();
-                cmd.Parameters.AddWithValue("@ma1", ma.ElementAtOrDefault(0) ?? string.Empty);
-                cmd.Parameters.AddWithValue("@ma2", ma.ElementAtOrDefault(1) ?? string.Empty);
-                cmd.Parameters.AddWithValue("@ma3", ma.ElementAtOrDefault(2) ?? string.Empty);
-                cmd.Parameters.AddWithValue("@ma4", ma.ElementAtOrDefault(3) ?? string.Empty);
-                cmd.Parameters.AddWithValue("@ma5", ma.ElementAtOrDefault(4) ?? string.Empty);
+                cmd.Parameters.AddWithValue("@Accords", frag.Accords ?? string.Empty);
+                cmd.Parameters.AddWithValue("@Perfumers", frag.Perfumers ?? string.Empty);
                 cmd.Parameters.AddWithValue("@Id", id);
 
                 rowsAffected = await cmd.ExecuteNonQueryAsync();
@@ -208,12 +240,10 @@ namespace WebAppComp3011.Controllers
             {
                 await cn.OpenAsync();
                 var cmd = cn.CreateCommand();
-                cmd.CommandText = @"INSERT INTO perfume100 (
-                    url, Perfume, Brand, Country, Gender, ""Rating Value"", ""Rating Count"", year,
-                    Top, Middle, Base, Perfumer1, Perfumer2, mainaccord1, mainaccord2, mainaccord3, mainaccord4, mainaccord5
+                cmd.CommandText = @"INSERT INTO perf100 (
+                    url, Perfume, Brand, Country, Gender, Rating, Year, Accords, Perfumers
                     ) VALUES (
-                    @FragUrl, @FragName, @Brand, @Country, @Gender, @RatingValue, @RatingCount, @Year,
-                    @Top, @Middle, @Base, @Perfumer1, @Perfumer2, @ma1, @ma2, @ma3, @ma4, @ma5
+                    @FragUrl, @FragName, @Brand, @Country, @Gender, @Rating, @Year, @Accords, @Perfumers
                     ); SELECT last_insert_rowid();";
 
                 cmd.Parameters.AddWithValue("@FragUrl", fragrance.FragUrl ?? string.Empty);
@@ -221,20 +251,10 @@ namespace WebAppComp3011.Controllers
                 cmd.Parameters.AddWithValue("@Brand", fragrance.Brand ?? string.Empty);
                 cmd.Parameters.AddWithValue("@Country", fragrance.Country ?? string.Empty);
                 cmd.Parameters.AddWithValue("@Gender", fragrance.Gender ?? string.Empty);
-                cmd.Parameters.AddWithValue("@RatingValue", fragrance.RatingValue);
-                cmd.Parameters.AddWithValue("@RatingCount", fragrance.RatingCount);
+                cmd.Parameters.AddWithValue("@Rating", fragrance.Rating);
                 cmd.Parameters.AddWithValue("@Year", fragrance.Year ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Top", fragrance.Notes?.Top ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Middle", fragrance.Notes?.Middle ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Base", fragrance.Notes?.Base ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Perfumer1", fragrance.Perfumer1 ?? string.Empty);
-                cmd.Parameters.AddWithValue("@Perfumer2", fragrance.Perfumer2 ?? string.Empty);
-                var ma = fragrance.MainAccords ?? new List<string>();
-                cmd.Parameters.AddWithValue("@ma1", ma.ElementAtOrDefault(0) ?? string.Empty);
-                cmd.Parameters.AddWithValue("@ma2", ma.ElementAtOrDefault(1) ?? string.Empty);
-                cmd.Parameters.AddWithValue("@ma3", ma.ElementAtOrDefault(2) ?? string.Empty);
-                cmd.Parameters.AddWithValue("@ma4", ma.ElementAtOrDefault(3) ?? string.Empty);
-                cmd.Parameters.AddWithValue("@ma5", ma.ElementAtOrDefault(4) ?? string.Empty);
+                cmd.Parameters.AddWithValue("@Accords", fragrance.Accords ?? string.Empty);
+                cmd.Parameters.AddWithValue("@Perfumers", fragrance.Perfumers ?? string.Empty);
 
                 var result = await cmd.ExecuteScalarAsync();
                 newId = Convert.ToInt32(result);
@@ -254,7 +274,7 @@ namespace WebAppComp3011.Controllers
             {
                 await cn.OpenAsync();
                 var cmd = cn.CreateCommand();
-                cmd.CommandText = "DELETE FROM perfume100 WHERE rowid = @id";
+                cmd.CommandText = "DELETE FROM perf100 WHERE perfumeId = @id";
                 cmd.Parameters.AddWithValue("@id", id);
                 rowsAffected = await cmd.ExecuteNonQueryAsync();
                 await cn.CloseAsync();
@@ -273,7 +293,7 @@ namespace WebAppComp3011.Controllers
             {
                 cn.Open();
                 using var cmd = cn.CreateCommand();
-                cmd.CommandText = "SELECT 1 FROM perfume100 WHERE rowid = @id LIMIT 1";
+                cmd.CommandText = "SELECT 1 FROM perf100 WHERE perfumeId = @id LIMIT 1";
                 cmd.Parameters.AddWithValue("@id", id);
                 var result = cmd.ExecuteScalar();
                 exists = result != null;
@@ -281,6 +301,22 @@ namespace WebAppComp3011.Controllers
 
             return exists;
         }
+
+        private int? TryGetInt(SqliteDataReader reader, string name)
+        {
+            try
+            {
+                if (reader.GetOrdinal(name) >= 0 && !reader.IsDBNull(reader.GetOrdinal(name)))
+                {
+                    return Convert.ToInt32(reader[name]);
+                }
+            }
+            catch { }
+            return null;
+        }
+
+        
+        */
     }
 }
 
